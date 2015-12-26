06a5278da32a3999d1d35195484681f148b6da09
gracie <gfang@wellesley.edu>
2015-12-26 01:36:04 -0800

    added nav bar for feed in main page

--- main.js
+++ main.js
@@ -5,4 +5,31 @@ $(document).ready(function() {
 	function main() {
 		window.location.replace('main.html');
 	}
+
+	$('#trends').click(function() {
+		//alert('trends clicked');
+		$('#trends').addClass('active');
+		$('#friends').removeClass('active');
+		$('#categories').removeClass('active');
+
+
+	})
+
+	$('#friends').click(function() {
+		// alert('friends clicked');
+		$('#friends').addClass('active');
+		$('#categories').removeClass('active');
+		$('#trends').removeClass('active');
+
+
+	})
+
+	$('#categories').click(function() {
+		//alert('categories clicked');
+		$('#categories').addClass('active');
+		$('#trends').removeClass('active');
+		$('#friends').removeClass('active');
+	})
+
+
 })
