c5c64f8ee35ae91fbede9c4145f20aca5598a669
gracie <gfang@wellesley.edu>
2016-01-03 01:14:28 -0800

    add event button and corresponding bootbox dialog box added

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	//start with just trends showing
 	$('.friends').hide();
 	$('.categories').hide();
+	// $('.dialog-body').hide();
 
 	$('#trends').click(function() {
 		//alert('trends clicked');
@@ -27,7 +28,6 @@ $(document).ready(function() {
 		$('.categories').hide();
 		$('.friends').show();
 
-
 	})
 
 	$('#categories').click(function() {
@@ -41,6 +41,34 @@ $(document).ready(function() {
 		$('.categories').show();
 
 	})
+
+
+
+	$('#add-event').click(function() {
+		bootbox.dialog({
+		  message: "When is your event?",
+		  title: "Add Event",
+		  buttons: {
+		    main: {
+		      label: "Publish!",
+		      className: "btn-primary",
+		      
+		    }
+		  }
+		});
+		
+		// $('.bootbox-body').append('<select class="form-control day"></select>');
+		$('.bootbox-body').addClass('input-group');
+		$('.input-group').append('<input type="text" class="form-control event-name" placeholder="Name of Event" aria-describedby="sizing-addon1">');
+		$('.bootbox-body').append(' <textarea class="form-control event-description" rows="6" placeholder="Add a short description of your event!"></textarea>');
+
+		// for(var i=1; i<31; i++) {
+		// 	var day = i.toString();
+		// 	$('.day').append("<option value=" + day + ">" + day + "</option>");
+		// }
+	})
+
+	
 
 	// $('.range-slider').jRange({
 	//     from: 18,
