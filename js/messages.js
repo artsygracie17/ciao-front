52dc3bdc8fc1d0c7ab7012a9fdd453c40666a184
gracie <gfang@wellesley.edu>
2016-01-10 20:51:02 -0800

    removed superfluous messages.js

--- js/messages.js
+++ js/messages.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
 
+	// $('.name').click(function(e) {
+	// 	console.log('clicked');
+	// 	clicked_id = e.target.id;
+	// 	console.log(e.target.id);
+	// 	$('.name').removeClass('active');
+	// 	$('#'+clicked_id.toString()).addClass('active');
+	// 	$('#name-of-person').html(clicked_id.toString());
+	// })
 
 })
